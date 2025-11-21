dbf5d3c763781144662469c9c597bcffd69a97b2
Kishore-1611 <kishorethodeti143@gmail.com>
2025-11-21 19:30:59 +0530

    After Deleting A,B,C

--- A.java
+++
@@ -1,5 +0,0 @@
-public class A {
-    void A1(){
-        System.out.println("A1() method")
-    }
-}
--- B.java
+++
@@ -1,5 +0,0 @@
-public class B {
-    void B1() {
-        System.out.println("B1() method");
-    }
-}
