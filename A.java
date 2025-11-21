71b4f36636aae60ac31cfc04868f49a00cabc089
Kishore-1611 <kishorethodeti143@gmail.com>
2025-11-21 19:20:29 +0530

    A,B Files Added

---
+++ A.java
@@ -0,0 +1,5 @@
+public class A {
+    void A1(){
+        System.out.println("A1() method")
+    }
+}
---
+++ B.java
@@ -0,0 +1,5 @@
+public class B {
+    void B1() {
+        System.out.println("B1() method");
+    }
+}
